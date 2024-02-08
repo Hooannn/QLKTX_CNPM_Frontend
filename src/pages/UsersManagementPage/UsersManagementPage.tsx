73557213db55e80ac2e91e5f744e8df4fee9200b
hoanthui <hoanthui123@gmail.com>
2024-02-08 21:42:01 +0700

    feat: update, delete, filter users

--- src/pages/UsersManagementPage/UsersManagementPage.tsx
+++ src/pages/UsersManagementPage/UsersManagementPage.tsx
@@ -11,12 +11,15 @@ import {
   Button,
   useDisclosure,
   Image,
+  Select,
+  SelectItem,
 } from "@nextui-org/react";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import useAxiosIns from "../../hooks/useAxiosIns";
 import { IResponseData, IUser } from "../../types";
 import CreateUserModal from "./CreateUserModal";
+import UserCellActions from "./UserCellActions";
 
 export default function UsersManagementPage() {
   const [page, setPage] = useState(1);
@@ -36,6 +39,22 @@ export default function UsersManagementPage() {
     onOpen: onOpenCreateUserModal,
     onClose: onCreateUserModalClose,
   } = useDisclosure();
+
+  const [selectedSex, setSelectedSex] = useState<string>("ALL");
+  const [selectedRole, setSelectedRole] = useState<string>("ALL");
+
+  const filterUsers = users
+    .filter((user) => {
+      if (selectedSex !== "ALL") return user.sex === selectedSex;
+      return true;
+    })
+    .filter((user) => {
+      if (selectedRole !== "ALL") return user.role === selectedRole;
+      return true;
+    });
+
+  const tableItems = filterUsers.slice((page - 1) * 10, page * 10);
+
   return (
     <>
       <CreateUserModal
@@ -45,29 +64,75 @@ export default function UsersManagementPage() {
       <div>
         <div className="flex items-center justify-between pb-4">
           <div className="text-lg font-bold">Quản lý người dùng</div>
-          <Button
-            onClick={onOpenCreateUserModal}
-            color="primary"
-            className="p-6"
-          >
-            Tạo mới
-          </Button>
+          <div className="flex gap-4">
+            <Select
+              onSelectionChange={(selection) => {
+                const keys = Array.from(selection) as string[];
+                setSelectedSex(keys[0]?.toString());
+              }}
+              className="w-40 h-12"
+              size="sm"
+              variant="bordered"
+              defaultSelectedKeys={["ALL"]}
+              label="Giới tính"
+            >
+              <SelectItem key="ALL" value="ALL">
+                Tất cả
+              </SelectItem>
+              <SelectItem key="MALE" value="MALE">
+                Nam
+              </SelectItem>
+              <SelectItem key="FEMALE" value="FEMALE">
+                Nữ
+              </SelectItem>
+              <SelectItem key="OTHER" value="OTHER">
+                Khác
+              </SelectItem>
+            </Select>
+            <Select
+              defaultSelectedKeys={["ALL"]}
+              size="sm"
+              className="w-40"
+              variant="bordered"
+              label="Quyền"
+              onSelectionChange={(selection) => {
+                const keys = Array.from(selection) as string[];
+                setSelectedRole(keys[0]?.toString());
+              }}
+            >
+              <SelectItem key="ALL" value="ALL">
+                Tất cả
+              </SelectItem>
+              <SelectItem key="STUDENT" value="STUDENT">
+                Sinh viên
+              </SelectItem>
+              <SelectItem key="STAFF" value="STAFF">
+                Quản lý
+              </SelectItem>
+            </Select>
+            <Button
+              onClick={onOpenCreateUserModal}
+              color="primary"
+              className="p-6"
+            >
+              Tạo mới
+            </Button>
+          </div>
         </div>
 
         <Table
           bottomContent={
-            users.length > 10 ? (
+            filterUsers.length > 10 ? (
               <div className="flex w-full justify-center">
                 <Pagination
                   isCompact
-                  showControls
                   showShadow
                   color="primary"
                   page={page}
                   total={
-                    users.length % 10 === 0
-                      ? users.length / 10
-                      : users.length / 10 + 1
+                    filterUsers.length % 10 === 0
+                      ? filterUsers.length / 10
+                      : filterUsers.length / 10 + 1
                   }
                   onChange={(page) => setPage(page)}
                 />
@@ -88,31 +153,29 @@ export default function UsersManagementPage() {
             <TableColumn key="actions">Thao tác</TableColumn>
           </TableHeader>
           <TableBody
-            items={users.slice((page - 1) * 10, page * 10)}
+            items={tableItems}
             emptyContent={
-              <div><Image removeWrapper className="mx-auto" width={250} src="/Empty.svg" />
-                <div><small>Hiện tại không có người dùng nào.</small></div></div>
+              <div>
+                <Image
+                  removeWrapper
+                  className="mx-auto"
+                  width={250}
+                  src="/Empty.svg"
+                />
+                <div>
+                  <small>Hiện tại không có người dùng nào.</small>
+                </div>
+              </div>
             }
             loadingContent={<Spinner />}
-            loadingState={
-              getUsersQuery.isLoading
-                ? "loading"
-                : undefined
-            }
+            loadingState={getUsersQuery.isLoading ? "loading" : undefined}
           >
             {(item) => (
               <TableRow key={item?.id}>
                 {(columnKey) => (
                   <TableCell>
                     {columnKey === "actions" ? (
-                      <div className="flex gap-1">
-                        <Button size="sm" color="primary" variant="light">
-                          Sửa
-                        </Button>
-                        <Button size="sm" color="danger" variant="flat">
-                          Xóa
-                        </Button>
-                      </div>
+                      <UserCellActions user={item} />
                     ) : (
                       <>
                         {getKeyValue(item, columnKey) ?? (
